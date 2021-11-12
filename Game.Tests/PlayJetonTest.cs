43483473d690cd255e899c8f28f0bc31c34b0641
Caroline Fassot <fassot.caroline@gmail.com>
2021-11-12 19:05:38 +0100

    supp commentaire

--- Game.Tests/PlayJetonTest.cs
+++ Game.Tests/PlayJetonTest.cs
@@ -19,20 +19,6 @@ namespace Game.Tests
             board.AddJeton(6, player1.currentPlayer);
             Assert.AreEqual(1, boardArray[6,6]);
         }
-
-        // [Test]
-        // public void checkComputerPlay()
-        // {
-        //     Board board = new Board();
-        //     Computer player2 = new Computer(2);
-
-        //     int[,] boardArray = Board._board;
-
-        //     int computerColumn = player2.play();
-
-        //     board.AddJeton(computerColumn, player2.currentPlayer);
-        //     Assert.AreEqual(2, boardArray[6,computerColumn]);
-        // }
     
     }
 }
